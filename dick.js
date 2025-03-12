d9f4e956a7f8122efc96241fa4b54e81ba54b792
priyanshuBS <priyanshuparashar08@gmail.com>
2025-03-12 12:05:23 +0530

    seperate html css and js

---
+++ dick.js
@@ -0,0 +1,10 @@
+const dickButton = document.querySelector('.js-dick-button');
+
+dickButton.addEventListener('click', () => {
+    const randomNumber = (Math.random() * 1000 % 8).toFixed(1);
+    document.querySelector('.js-result').innerHTML = `Your dick size is ${randomNumber} inches`;
+});
+
+dickButton.addEventListener('blur', () => {
+    document.querySelector('.js-result').innerHTML = "";
+});
